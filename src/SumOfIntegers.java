0816debbabda5aa729e251656afe1061314fc449
AdHominem <vargsmane@gmx.de>
2016-05-05 11:34:50 +0200

    Improved and refactored SumOfIntegers

--- src/SumOfIntegers.java
+++ src/SumOfIntegers.java
@@ -3,8 +3,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
+import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
+
+/**
+ *  A program to determine the largest sum of contiguous integers in a list.
+ *  The first argument is a path to a filename containing a comma-separated list of integers, one per line.
+ *  The largest sum is printed to stdout. In other words, of all the possible contiguous subarrays for a given array,
+ *  the one with the largest sum is found and printed.
+ */
 public class SumOfIntegers {
 
     public static void main(String[] args) throws IOException {
@@ -12,22 +20,62 @@ public class SumOfIntegers {
         Files.newBufferedReader(Paths.get(args[0])).lines()
                                                     .map(SumOfIntegers::turnToIntegerList)
                                                     .map(SumOfIntegers::getAllContiguousSubarrays)
+                                                    .map(SumOfIntegers::getSumsOfSubarrays)
+                                                    .mapToInt(Collections::max)
                                                     .forEach(System.out::println);
     }
 
-    private static List<Integer> getAllContiguousSubarrays(List<Integer> integers) {
-        return integers;
-    }
-
     private static List<Integer> turnToIntegerList(String line) {
-        List<String> lineAsList = Arrays.asList(line.split(","));
-        return getAsIntegerList(lineAsList).stream()
-                                           .collect(toList());
+
+        return asList(line.split(",")).stream()
+                                      .map(Integer::valueOf)
+                                      .collect(toList());
     }
 
-    private static List<Integer> getAsIntegerList(List<String> stringList) {
-        return stringList.stream()
-                         .map(Integer::valueOf)
-                         .collect(toList());
+    /**
+     * Returns all possible contiguous subarrays for a given List. This method calls getEndTruncatedSubListsOf each
+     * different frontTruncatedSubList, yielding all possible front and end truncated lists - which are all subarrays.
+     * @param integerList A List containing Integer values.
+     * @return A List of all subarrays as Lists.
+     */
+    private static List<List<Integer>> getAllContiguousSubarrays(List<Integer> integerList) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (List<Integer> frontTruncatedSubList : getFrontTruncatedSubListsOf(integerList)) {
+            result.addAll(getEndTruncatedSubListsOf(frontTruncatedSubList));
+        }
+
+        return result;
     }
+
+    private static List<List<Integer>> getFrontTruncatedSubListsOf(List<Integer> list) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            result.add(list.subList(i, list.size()));
+        }
+
+        return result;
+    }
+
+    private static List<List<Integer>> getEndTruncatedSubListsOf(List<Integer> list) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            result.add(list.subList(0, list.size() - i));
+        }
+
+        return result;
+    }
+
+    private static List<Integer> getSumsOfSubarrays(List<List<Integer>> subarrays) {
+        return subarrays.stream()
+                .map(subsubarray -> subsubarray.stream()
+                                                .mapToInt(integer -> integer)
+                                                .sum())
+                .collect(toList());
+    }
+
+
+
 }
