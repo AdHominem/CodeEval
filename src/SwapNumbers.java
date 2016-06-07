6ab73616f202d89f105bb9270fd34319a8f8824a
AdHominem <vargsmane@gmx.de>
2016-06-07 19:35:53 +0200

    Some more

--- src/SwapNumbers.java
+++ src/SwapNumbers.java
@@ -9,20 +9,14 @@ public class SwapNumbers {
 
     public static void main(String[] args) throws IOException {
 
-
-        String token = "dfgd[1:6]";
-        System.out.println(token.matches("(.*)\\[\\d\\:\\d\\](.*)"));
-
-        String token2 = "import java.util.stream.*;";
-        System.out.println(token2.contains("java"));
-//        try (BufferedReader input = Files.newBufferedReader(Paths.get(args[0]))) {
-//            input.lines()
-//                 .map(line -> Arrays.asList(line.split(" ")).stream()
-//                                                .map(word -> word.charAt(word.length()-1)
-//                                                        + word.substring(1,word.length()-1)
-//                                                        + word.charAt(0))
-//                                                .collect(Collectors.joining(" ")))
-//                 .forEach(System.out::println);
-//        }
+        try (BufferedReader input = Files.newBufferedReader(Paths.get(args[0]))) {
+            input.lines()
+                 .map(line -> Arrays.asList(line.split(" ")).stream()
+                                                .map(word -> word.charAt(word.length()-1)
+                                                        + word.substring(1,word.length()-1)
+                                                        + word.charAt(0))
+                                                .collect(Collectors.joining(" ")))
+                 .forEach(System.out::println);
+        }
     }
 }
